95433a986a8c6a631423e5b9e80ef71e6014d61f
Artem Pavlov <pavlov.tema@gmail.com>
2021-09-11 15:06:03 +0500

    Model Person created.

--- PersonsList/Models/Person.swift
+++ PersonsList/Models/Person.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Artem Pavlov on 08.09.2021.
 //
+import  Foundation
 
 struct Person {
     let name: String
@@ -11,15 +12,49 @@ struct Person {
     let numberPhone: String
     let email: String
     
-    static func getPerson(nameOfPerson: String, secondNameOfPerson: String, numberPhoneOfPerson: String, emailOfPerson: String) -> [Person] {
-        return []
-        }
-        
-
-    
+    var fullName: String {
+        "\(name) \(secondName)"
     }
-    
-    
-    
+}
+
+// заполнение массива случайными не повторяющимися данными
+extension Person {
+    static func getContactList() -> [Person] {
+        
+        var persons: [Person] = []
+        
+        let names = DataManager.shared.realNames.shuffled()
+        let secondsNames = DataManager.shared.realSecondNames.shuffled()
+        let phoneNumbers = DataManager.shared.realNumbersPhones.shuffled()
+        let emails = DataManager.shared.realEmails.shuffled()
+        
+        // в нашем случае iterationCount не имеет смысла, так как у нас во всех массивах число элементов равно. Если бы было разным, то можно использовать наименьший индекс
+        
+        let iterationCount = min(
+            names.count,
+            secondsNames.count,
+            phoneNumbers.count,
+            emails.count
+        )
+        
+        for index in 0..<iterationCount {
+            let person = Person(
+                name: names[index],
+                secondName: secondsNames[index],
+                numberPhone: phoneNumbers[index],
+                email: emails[index]
+            )
+            persons.append(person)
+        }
+     return persons
+    }
+}
+
+enum Contacts: String {
+    case phone = "phone" //название кнопочек, которые используем на втором экране
+    case email = "tray" //название кнопочек
+}
+
+
 
 
