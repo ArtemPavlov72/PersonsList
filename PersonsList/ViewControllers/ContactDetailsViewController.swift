57f9849cad832e01104a8e7c7ff81104187291cb
Artem Pavlov <pavlov.tema@gmail.com>
2021-09-11 16:24:50 +0500

    Segue from Cell (from ContactListViewController to ContactDetailsViewController) ContactDetailsViewController finished. TabBarController init.

--- PersonsList/ViewControllers/ContactDetailsViewController.swift
+++ PersonsList/ViewControllers/ContactDetailsViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class ContactDetailsViewController: UIViewController {
 
+    @IBOutlet var phoneLabel: UILabel!
+    @IBOutlet var emailLabel: UILabel!
+    
+    var person: Person!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        title = person.fullName
+        emailLabel.text = "Email: \(person.email)"
+        phoneLabel.text = "Phone: \(person.numberPhone)"
     }
     
 
